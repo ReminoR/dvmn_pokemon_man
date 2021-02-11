fab418f38fedd6c69d559532b6cf050f3c022753
Victor Shamakov <sva1.0@mail.ru>
2021-02-11 15:36:34 +0700

    Выполнено 8 задание

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # your models here
+class Pokemon(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='pokemons', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+class PokemonEntity(models.Model):
+    Pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    lon = models.FloatField()
+    lat = models.FloatField()
+
+    def __str__(self):
+        return f"{self.lat} {self.lon}"
