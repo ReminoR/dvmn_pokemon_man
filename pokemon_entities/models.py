b06a6ebb51fb9190612142f19ac84a013ecafded
Victor Shamakov <sva1.0@mail.ru>
2021-02-16 17:33:54 +0700

    Добавлены verbose_name. Понижены требования к заполнению данных

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,28 +2,29 @@ from django.db import models
 
 # your models here
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, null=True, blank=True)
-    title_jp = models.CharField(max_length=200, null=True, blank=True)
-    description = models.TextField(default='Описание покемона')
-    image = models.ImageField(upload_to='pokemons', null=True, blank=True)
-    previous_evolution = models.ForeignKey(
-        'Pokemon', related_name='next_evolutions', null=True, blank=True, on_delete=models.SET_NULL)
+    title = models.CharField(verbose_name='Имя', max_length=200)
+    title_en = models.CharField(verbose_name='Имя на английском', max_length=200, null=True, blank=True)
+    title_jp = models.CharField(verbose_name='Имя на японском', max_length=200, null=True, blank=True)
+    description = models.TextField(verbose_name='Описание', default='Описание покемона', blank=True)
+    image = models.ImageField(verbose_name='Фото', upload_to='pokemons', null=True, blank=True)
+    previous_evolution = models.ForeignKey('Pokemon', 
+        verbose_name = 'Предыдущая эволюция', related_name='next_evolutions',
+        null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.title}"
 
 class PokemonEntity(models.Model):
-    Pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lon = models.FloatField()
-    lat = models.FloatField()
-    appeared_at = models.DateTimeField(null=True)
-    disappeared_at = models.DateTimeField(null=True)
-    level = models.IntegerField()
-    health = models.IntegerField()
-    strength = models.IntegerField()
-    defence = models.IntegerField()
-    stamina = models.IntegerField()
+    Pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', on_delete=models.CASCADE)
+    lon = models.FloatField(verbose_name='Долгота')
+    lat = models.FloatField(verbose_name='Широта')
+    appeared_at = models.DateTimeField(verbose_name='Появится', null=True)
+    disappeared_at = models.DateTimeField(verbose_name='Исчезнет', null=True)
+    level = models.IntegerField(verbose_name='Уровень', blank=True)
+    health = models.IntegerField(verbose_name='Здоровье', blank=True)
+    strength = models.IntegerField(verbose_name='Сила', blank=True)
+    defence = models.IntegerField(verbose_name='Защита', blank=True)
+    stamina = models.IntegerField(verbose_name='Выносливость', blank=True)
 
     def __str__(self):
         return f"{self.lat} {self.lon}"
